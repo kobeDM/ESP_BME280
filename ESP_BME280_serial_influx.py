dac2ece1a77e2456e5f953bb8aa91ec4e147f427
Satoshi Higashino <higashino@people.kobe-u.ac.jp>
2025-05-14 20:08:20 +0900

    remove some comments

--- ESP_BME280_serial_influx.py
+++ ESP_BME280_serial_influx.py
@@ -6,7 +6,6 @@ import os,sys
 import argparse
 import json
 import re
-#import pyvisa as visa
 
 from serial.tools import list_ports
 from time import sleep
@@ -25,12 +24,9 @@ def ESP_search(ports):
         if str(reply)[0:len(DEV_ID)] == DEV_ID :
             print(DEV_ID+" found")
             return ports[i]
-#        else:
-#            print("")
     return -1
 
 
-#ID=0x7f36e066ab38
 int_def=10
 parser = argparse.ArgumentParser(description='BME280 monitoring tool')
 parser.add_argument("-o",help="one shot flag",action='store_true')
@@ -40,10 +36,8 @@ args=parser.parse_args();
 interval=args.i
 oneshot=args.o
 
+# csv output not inplemented at this moment...
 config_output = 'both'
-
-stadir='./'
-#stadir=os.environ['HOME']+'/status/BME280/'
 ocsv, odb = True, True
 if config_output == 'db':
     ocsv, odb = False, True
@@ -54,68 +48,47 @@ sql_dbname='BME280'
                
 if odb:
     from influxdb import InfluxDBClient
-    client = InfluxDBClient( host     = "10.37.0.212",port     = "8086",username = "root",password = "root",database= "BME280_1" )
+    client = InfluxDBClient( host     = "localhost",port     = "8086",username = "root",password = "root",database= "KUBEAM" )
 
 print ("ESP_BME280 serial reader")
 print ("device ID=",DEV_ID)
 
 ports=list_ports.comports();
-#print(ports)
-#device=[info.description for info in ports]
 
 ser=serial.Serial()
 ser.baud=baud
-#device=[info for info in ports if "CP2102N" in info.description]
 device=[info.device for info in ports if "CP2102N" in info.description]
 print(device)
 if len(device)==0:
     print("device not found")
     exit(0)
     
-#ser.
 port=device[0]
-#ports=port_search() #search for active ports
-#print(ports)
-
-
-#port=ESP_search(ports)
-#print(str(DEV_ID)+" USB port="+str(port))
-#exit(0)
-#ser=serial.Serial('/dev/ttyUSB0',baud)
-
-    
 while 1:
-#while 0:
-#    ser.open()
     ser=serial.Serial(port,baud)
     line=ser.readline().decode().replace('\n','')
     lines=line.split()
     dt=datetime.datetime.today()
     print(dt.strftime('%Y/%m/%d/%H:%M:%S'),"\t",line)
-    #print(lines[0])
-    #print(line.decode())
-    
-    if odb:
-        #db_data = [datetime.datetime.utcnow()]
 
-        
+    if odb:
         json_data = [
             {
-                'measurement' : 'env',
+                'measurement' : 'bme',
                 'fields' : {
-                    'sensor'  : lines[0],
-                    'pre'  : float(lines[1]),
-                    'temp'  : float(lines[2]),
-                    'hum'  : float(lines[3])
+                    'sensor' : lines[0],
+                    'pre'    : float(lines[1]),
+                    'temp'   : float(lines[2]),
+                    'hum'    : float(lines[3])
                 },
                 'time' : datetime.datetime.utcnow(),
                 'tags' : {
-                    'device' : 'BME280_1'
+                    'device' : 'BME280',
+                    'host'   : 'na177'
                 }
             }
         ]   
         result = client.write_points(json_data)
-        #cursor.execute("insert into BME280_1(sensor,pre,temp,hum) values(%s,%s,%s,%s)",(lines[0],lines[1],lines[2],lines[3]))
         
     ser.close()
     if oneshot:
@@ -123,8 +96,3 @@ while 1:
     sleep(interval)
 
 
-
-
-
-    
-    #ser=
